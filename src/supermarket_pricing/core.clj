91d2d1178f8f84b34625b6946156bb84ea7eae40
Ben Davies <ben.davies@audiogum.com>
2020-03-02 19:37:04 +0000

    Basic receipt calculator

--- src/supermarket_pricing/core.clj
+++ src/supermarket_pricing/core.clj
@@ -5,3 +5,26 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def prices
+  {"beans" 0.5
+   "coke"  0.7})
+
+(def example-receipt
+  [{:name "beans" :quantity 3} {:name "coke" :quantity 1}])
+
+(defn create-receipt
+  [no-beans no-coke]
+  [{:name "beans" :quantity no-beans} {:name "coke" :quantity no-coke}])
+
+(defn price-of-item
+  [item]
+  (* (get prices (:name item)) (:quantity item)))
+
+(defn price-of-basket
+  [receipt]
+  (->> receipt
+       (map price-of-item)
+       (apply +)
+       (format "%.2f")
+       (str "£")))
