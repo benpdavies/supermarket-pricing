ca95c1c21bd4e0f7a9a577f6ee4bf264ed9416c4
Ben Davies <ben.davies@audiogum.com>
2020-03-02 19:42:42 +0000

    Simple calculator test

--- test/supermarket_pricing/core_test.clj
+++ test/supermarket_pricing/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [supermarket-pricing.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest price-of-basket-is-calculated-correctly
+  (testing "Price of basket calculator"
+    (is (= "£3.40" (price-of-basket (create-receipt 4 2))))))
